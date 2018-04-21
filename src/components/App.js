458a7c852af3065d56ebda29e3e0ecb54f8de084
jooh <mnh513@naver.com>
2018-04-21 17:50:36 +0900

    add loading page

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
+import { Loader } from 'semantic-ui-react'
 
 import NavigationBar from './app/NavigationBar'
+import Loadingpage from './app/Loadingpage'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading : true,
+      intervalId : '',
+      count : 3,
+    }
+
+    this.loadingTimer = this.loadingTimer.bind(this);
+  }
+
+  componentDidMount() {
+    var intervalId = setInterval(this.loadingTimer, 1000);
+    this.setState({
+      intervalId: intervalId,
+    });
+  }
+
+  loadingTimer() {
+    this.setState({ count : this.state.count - 1 });
+
+    if(this.state.count <= 0) {
+      clearInterval(this.state.intervalId);
+      this.setState({ loading : false });
+    }
+  }
+
   render() {
-    return (
-      <div>
-        <NavigationBar />
-        {this.props.children}
-      </div>
-    );
+    if(this.state.loading)
+      return (
+        <Loadingpage count = {this.state.count}/>
+      );
+
+    else
+      return (
+        <div>
+          <NavigationBar />
+          {this.props.children}
+        </div>
+      );
   }
 }
 
